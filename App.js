c8bdcd05b06d62bf6f551146f0451efa87e257f5
AlanDev <ahgonzalez433@gmail.com>
2024-04-12 16:00:04 -0600

    Se realizo el menu del To-Do list

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Menu from './components/Menu';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hola Mundo!</Text>
+      <Text>App To-Do List</Text>
       <StatusBar style="auto" />
     </View>
   );
